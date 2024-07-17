9eb2c6fb70f1b1e9398d5ca27c94247f57970b45
Ankush Kumar Jha <jhaankushcpp@gmail.com>
2024-07-17 11:07:46 +0530

    Update providers.tf

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -1,9 +1,3 @@
-#Provider:----------------------------------
-# provider "aws" {
-#   access_key = "AKIA3FLDZBWJP3TEXQGR"
-#   secret_key = "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"
-# }
-
 terraform {
   backend "s3" {
     bucket = "tf-bucket-tfstate"
